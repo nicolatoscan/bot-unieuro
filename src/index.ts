26d2d0e058fb9e6f8abe311dc840fe9cff86224a
Marco <marcobernardi175@gmail.com>
2021-03-26 11:47:31 +0100

    GPU Name & tg messages format updated

--- src/index.ts
+++ src/index.ts
@@ -20,28 +20,28 @@ if (!BOT_TOKEN) {
 
 const bot = new Telegraf(BOT_TOKEN)
 
-enum ELinkType { UNIEURO, MEDIAWORLD }
+enum ELinkType { UNIEURO, MEDIAWORLD, NEXTHS }
 
 const listToCheck: [ELinkType, string, string][] = [
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-993279/asus-tuf-geforce-gtx-1660-6gb", "GTX 1660 ASUS TUF"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119330/msi-geforce-gtx-1660-6gb", "GTX 1660 MSI"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119331/msi-geforce-gtx-1660-6gb-xs", "GTX 1660 MSI XS"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119329/msi-geforce-gtx-1660-super-6gb-xs", "GTX 1660 MSI Super XS"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-144417/msi-geforce-gtx-1660-6gb", "GTX 1660 MSI"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119327/msi-geforce-gtx-1660ti-6gb-xs", "GTX 1660TI MSI"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-991819/asus-phoenix-geforce-rtx-2060-6gb", "RTX 2060 ASUS"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119343/msi-radeon-rx-5700-xt-8gb", "RX 5700XT MSI"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-144430/msi-radeon-rx-5700-xt-8gb", "RX 5700XT MSI"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-153357/asus-tuf-geforce-rtx-3060-12gb", "RTX 3060 ASUS"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-152810/asus-rog-strix-geforce-rtx-3060-12gb", "RTX 3060 ASUS ROG"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-149212/asus-dual-geforce-rtx-3060-ti-8gb", "RTX 3060Ti"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-100380/evga-geforce-rtx-2070-8gb", "RTX 2070 EVGA"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-145091/asus-tuf-geforce-rtx-3070-8gb", "RTX 3070 ASUS"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-140082/asus-tuf-geforce-rtx-3080-10gb", "RTX 3080 ASUS"],
-    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-149220/asus-rog-strix-geforce-rtx-3070-8gb", "RTX 3070 ASUS"],
-    [ ELinkType.UNIEURO, "https://www.unieuro.it/online/Schede-grafiche/DUAL-RTX3060TI-O8G-pidASU60YV0G12M0NA00", "RTX 3060"],
-    [ ELinkType.UNIEURO, "https://www.unieuro.it/online/Schede-grafiche/DUAL-RTX3070-O8G-pidASU90YV0FQ0M0NA00", "RTX 3070"],
-    [ ELinkType.UNIEURO, "https://www.unieuro.it/online/Schede-grafiche/TUF-RTX3080-O10G-GAMING-pidASU90YV0FB1M0NM00", "RTX 3080"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-993279/asus-tuf-geforce-gtx-1660-6gb", "ASUS TUF GeForce GTX 1660 6Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119330/msi-geforce-gtx-1660-6gb", "MSI GeForce GTX 1660 6Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119331/msi-geforce-gtx-1660-6gb-xs", "MSI GeForce GTX 1660 6Gb XS"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119329/msi-geforce-gtx-1660-super-6gb-xs", "MSI GeForce GTX 1660 Super 6Gb XS"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-144417/msi-geforce-gtx-1660-6gb", "MSI GeForce GTX 1660 6Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119327/msi-geforce-gtx-1660ti-6gb-xs", "MSI GeForce GTX 1660TI 6Gb XS"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-991819/asus-phoenix-geforce-rtx-2060-6gb", "ASUS Phoenix GeForce RTX 2060 6Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-119343/msi-radeon-rx-5700-xt-8gb", "MSI Radeon RX 5700 XT 8Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-144430/msi-radeon-rx-5700-xt-8gb", "MSI Radeon RX 5700 XT 8Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-153357/asus-tuf-geforce-rtx-3060-12gb", "ASUS TUF GeForce RTX 3060 12Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-152810/asus-rog-strix-geforce-rtx-3060-12gb", "ASUS ROG STRIX GeForce RTX 3060 12Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-149212/asus-dual-geforce-rtx-3060-ti-8gb", "ASUS Dual GeForce RTX 3060 Ti 8Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-100380/evga-geforce-rtx-2070-8gb", "EVGA GeForce® RTX 2070 8Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-145091/asus-tuf-geforce-rtx-3070-8gb", "ASUS TUF GeForce RTX 3070 8Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-140082/asus-tuf-geforce-rtx-3080-10gb", "ASUS TUF GeForce RTX 3080 10Gb"],
+    [ ELinkType.MEDIAWORLD, "https://www.mediaworld.it/product/p-149220/asus-rog-strix-geforce-rtx-3070-8gb", "ASUS ROG STRIX GeForce RTX 3070 8Gb"],
+    [ ELinkType.UNIEURO, "https://www.unieuro.it/online/Schede-grafiche/DUAL-RTX3060TI-O8G-pidASU60YV0G12M0NA00", "ASUS Dual -RTX3060TI-O8G "],
+    [ ELinkType.UNIEURO, "https://www.unieuro.it/online/Schede-grafiche/DUAL-RTX3070-O8G-pidASU90YV0FQ0M0NA00", "ASUS Dual -RTX3070-O8G"],
+    [ ELinkType.UNIEURO, "https://www.unieuro.it/online/Schede-grafiche/TUF-RTX3080-O10G-GAMING-pidASU90YV0FB1M0NM00", "ASUS TUF Gaming TUF-RTX3080-O10G"],
 ];
 
 let finito = false;
@@ -75,11 +75,23 @@ async function checkEl(type: ELinkType, url: string, name: string) {
 
         let sendingAttempt = 10;
         while (sendingAttempt > 0) {
-            const r = await sendMsgFound(name, url);
-            await delay(500)
-            sendingAttempt--;
-            if (r)
-                sendingAttempt = 0;
+            let r;
+            switch (type) {
+                case ELinkType.MEDIAWORLD:
+                    r = await sendMsgFoundMedia(name, url);
+                    await delay(500)
+                    sendingAttempt--;
+                    if (r)
+                        sendingAttempt = 0;
+                    break;
+                case ELinkType.UNIEURO:
+                    r = await sendMsgFoundMedia(name, url);
+                    await delay(500)
+                    sendingAttempt--;
+                    if (r)
+                        sendingAttempt = 0;
+                    break;
+            }
         }
 
         console.log(`Trovata la ${name}`);
@@ -91,7 +103,15 @@ async function checkEl(type: ELinkType, url: string, name: string) {
 
 async function sendMsgFound(name: string, url: string): Promise<boolean> {
     try {
-        await bot.telegram.sendMessage(CHANNEL_ID ?? '', `Sono disponibili le ${name} Da Unieuro accorri.\n\n${url}`);
+        await bot.telegram.sendMessage(CHANNEL_ID ?? '', `🏬Unieuro\n\n✅👉${name}\n\n📎[Comprala QUI](${url})`,{parse_mode: "MarkdownV2"})
+        return true;
+    } catch {
+        return false;
+    }
+}
+async function sendMsgFoundMedia(name: string, url: string): Promise<boolean> {
+    try {
+        await bot.telegram.sendMessage(CHANNEL_ID ?? '', `🏬Mediaworld\n\n✅👉${name}\n\n📎[Comprala QUI](${url})`,{parse_mode: "MarkdownV2"})
         return true;
     } catch {
         return false;
