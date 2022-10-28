65b34e1b14330fbed07d08130feafe4e855c82c0
euberdeveloper <euberdeveloper@gmail.com>
2022-10-28 12:11:36 +0200

    feat(index.ts): move list of links to check to json file #2
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { Telegraf } from 'telegraf'
 import axios from 'axios'
 import dotenv from 'dotenv'
 import { parse } from 'node-html-parser';
+import * as fs from 'fs';
+import * as path from 'path';
 dotenv.config()
 
-const WAIT_ON_TROVATA = 3*60*1000
-const WAIT_ON_NON_TROVATA = 2*1000
+const WAIT_ON_TROVATA = 3 * 60 * 1000
+const WAIT_ON_NON_TROVATA = 2 * 1000
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 const CHANNEL_ID = process.env.CHANNEL_ID
@@ -22,27 +24,9 @@ const bot = new Telegraf(BOT_TOKEN)
 
 type ELinkType = 'unieuro' | 'mediaworld' | 'nexths';
 
-const listToCheck: [ELinkType, string, string][] = [
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-993279/asus-tuf-geforce-gtx-1660-6gb", "ASUS TUF GeForce GTX 1660 6Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-119330/msi-geforce-gtx-1660-6gb", "MSI GeForce GTX 1660 6Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-119331/msi-geforce-gtx-1660-6gb-xs", "MSI GeForce GTX 1660 6Gb XS"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-119329/msi-geforce-gtx-1660-super-6gb-xs", "MSI GeForce GTX 1660 Super 6Gb XS"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-144417/msi-geforce-gtx-1660-6gb", "MSI GeForce GTX 1660 6Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-119327/msi-geforce-gtx-1660ti-6gb-xs", "MSI GeForce GTX 1660TI 6Gb XS"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-991819/asus-phoenix-geforce-rtx-2060-6gb", "ASUS Phoenix GeForce RTX 2060 6Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-119343/msi-radeon-rx-5700-xt-8gb", "MSI Radeon RX 5700 XT 8Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-144430/msi-radeon-rx-5700-xt-8gb", "MSI Radeon RX 5700 XT 8Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-153357/asus-tuf-geforce-rtx-3060-12gb", "ASUS TUF GeForce RTX 3060 12Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-152810/asus-rog-strix-geforce-rtx-3060-12gb", "ASUS ROG STRIX GeForce RTX 3060 12Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-149212/asus-dual-geforce-rtx-3060-ti-8gb", "ASUS Dual GeForce RTX 3060 Ti 8Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-100380/evga-geforce-rtx-2070-8gb", "EVGA GeForce® RTX 2070 8Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-145091/asus-tuf-geforce-rtx-3070-8gb", "ASUS TUF GeForce RTX 3070 8Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-140082/asus-tuf-geforce-rtx-3080-10gb", "ASUS TUF GeForce RTX 3080 10Gb"],
-    [ 'mediaworld', "https://www.mediaworld.it/product/p-149220/asus-rog-strix-geforce-rtx-3070-8gb", "ASUS ROG STRIX GeForce RTX 3070 8Gb"],
-    [ 'unieuro', "https://www.unieuro.it/online/Schede-grafiche/DUAL-RTX3060TI-O8G-pidASU60YV0G12M0NA00", "ASUS Dual -RTX3060TI-O8G "],
-    [ 'unieuro', "https://www.unieuro.it/online/Schede-grafiche/DUAL-RTX3070-O8G-pidASU90YV0FQ0M0NA00", "ASUS Dual -RTX3070-O8G"],
-    [ 'unieuro', "https://www.unieuro.it/online/Schede-grafiche/TUF-RTX3080-O10G-GAMING-pidASU90YV0FB1M0NM00", "ASUS TUF Gaming TUF-RTX3080-O10G"],
-];
+let listToCheck: [ELinkType, string, string][] = getListToCheck();
+const syncSeconds = 15;
+setInterval(() => { listToCheck = getListToCheck(); }, 15 * 1000);
 
 let finito = false;
 process.once('SIGINT', () => { bot.stop('SIGINT'); finito = true; })
@@ -50,6 +34,12 @@ process.once('SIGTERM', () => { bot.stop('SIGTERM'); finito = true; })
 listToCheck.forEach(el => {
     lavoraPerEl(el[0], el[1], el[2]);
 })
+
+function getListToCheck() {
+    const filePath = path.join(__dirname, 'list.json');
+    const text = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(text);
+}
 
 
 async function lavoraPerEl(type: ELinkType, url: string, name: string) {
@@ -60,7 +50,7 @@ async function lavoraPerEl(type: ELinkType, url: string, name: string) {
 
 async function checkEl(type: ELinkType, url: string, name: string) {
     console.log(`Checking ${name}`);
-    
+
     let res = false;
     switch (type) {
         case 'mediaworld':
@@ -103,7 +93,7 @@ async function checkEl(type: ELinkType, url: string, name: string) {
 
 async function sendMsgFoundMedia(name: string, url: string): Promise<boolean> {
     try {
-        await bot.telegram.sendMessage(CHANNEL_ID ?? '', `🏬Mediaworld\n\n✅👉${name}\n\n📎[Comprala QUI](${url})`,{parse_mode: "MarkdownV2"})
+        await bot.telegram.sendMessage(CHANNEL_ID ?? '', `🏬Mediaworld\n\n✅👉${name}\n\n📎[Comprala QUI](${url})`, { parse_mode: "MarkdownV2" })
         return true;
     } catch {
         return false;
